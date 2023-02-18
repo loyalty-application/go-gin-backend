26b7be9015e81728d9a0502fc1f2daf5dd8eb4ff
soonann <sudo@soonann.dev>
2023-02-19 01:27:05 +0800

    fix: removed sample code for postgresql gin setup

--- main.go
+++ main.go
@@ -1,33 +1,19 @@
 package main
 
 import (
-	"github.com/cs301-itsa/project-2022-23t2-project-2022-23t2-g1-t6/controllers"
-	"github.com/cs301-itsa/project-2022-23t2-project-2022-23t2-g1-t6/initializers"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-func init() {
-	initializers.LoadEnvVariables()
-	initializers.ConnectToDatabase()
-}
-
 func main() {
-	r := gin.Default()
-
-	// show all posts
-	r.GET("/posts", controllers.PostsIndex)
-
-	// show specific post with specific id
-	r.GET("/posts/:id", controllers.PostsShow)
-
-	// create new post
-	r.POST("/posts", controllers.PostsCreate)
-
-	// update post
-	r.PUT("/posts/:id", controllers.PostsUpdate)
-
-	// delete request
-	r.DELETE("/posts/:id", controllers.PostsDelete)
-
-	r.Run() // listen and serve on 0.0.0.0:8080
+    r := gin.Default()
+    
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+    
 }
--- start-server.sh
+++
@@ -1,2 +0,0 @@
-# starts server with compile daemon to monitor changes in source code
-CompileDaemon -build="go build -o ./bin/project-2022-23t2-project-2022-23t2-g1-t6-rideshare-api" -command="./bin/project-2022-23t2-project-2022-23t2-g1-t6-rideshare-api" 
