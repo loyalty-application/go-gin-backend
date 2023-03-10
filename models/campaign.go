7b7ccc86db1e8baa5336f9c6546e6a83d5bfc1cb
Ryan Hong <ryanhong1126@gmail.com>
2023-03-10 14:01:19 +0800

    formatting issue resolved

--- models/campaign.go
+++ models/campaign.go
@@ -1,8 +1,8 @@
 package models
 
 type Campaign struct {
-	Id int `json:"id" bson:"id" example:"1"`
-	Merchant string `json:"merchant" bson:"merchant" example:"7-11"`
-	CardType string  `json:"card_type" bson:"card_type" example"super_miles_card"`
+	Id          int    `json:"id" bson:"id" example:"1"`
+	Merchant    string `json:"merchant" bson:"merchant" example:"7-11"`
+	CardType    string `json:"card_type" bson:"card_type" example"super_miles_card"`
 	Description string `json:"description" bson:"description"`
 }
