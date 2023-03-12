e9c37cebcf2e2439082ab4a25c1dfce9a8e8f34f
Ryan Hong <ryanhong1126@gmail.com>
2023-03-12 22:55:02 +0800

    base for crud for campaign

--- controllers/campaign.go
+++ controllers/campaign.go
@@ -15,3 +15,31 @@ type CampaignController struct{}
 func (t CampaignController) GetCampaigns(c *gin.Context){
 
 }
+
+func (t CampaignController) GetCampaignId(c *gin.Context){
+
+}
+
+func (t CampaignController) CreateCampaign(c *gin.Context){
+    var campaign Campaign
+
+    if err := c.BindJSON(&campaign); err != nil {
+        c.AbortWithStatusJSON(400, gin.H{"error": err.Error()})
+        return
+    }
+
+    if err := ctrl.service.CreateCampaign(&campaign); err != nil {
+        c.JSON(http.StatusBadRequest, models.HTTPError{http.StatusBadRequest, "Invalid Transaction Object" + err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, campaign)
+}
+
+func (t CampaignController) UpdateCampaign(c *gin.Context){
+
+}
+
+func (t CampaignController) DeleteCampaign(c *gin.Context){
+
+}
