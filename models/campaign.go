0d4eee4fe5229d1363459354bc1ee48bb00987e4
hongyao38 <hongyao.lim.2021@scis.smu.edu.sg>
2023-04-04 20:39:48 +0000

    reverted back to string for campaign dates

--- models/campaign.go
+++ models/campaign.go
@@ -7,14 +7,14 @@ type CampaignList struct {
 }
 
 type Campaign struct {
-	UserId       string    `json:"-" bson:"user_id" example:"6400a..."`
-	CampaignId   string    `json:"campaign_id" bson:"campaign_id" example:"cmp00001"`
-	Merchant     string    `json:"merchant" bson:"merchant" example:"7-11"`
-	CardType     string    `json:"card_type" bson:"card_type" example:"super_miles_card"`
-	Description  string    `json:"description" bson:"description"`
-	StartDate    time.Time `json:"start_date" bson:"start_date"`
-	EndDate      time.Time `json:"end_date" bson:"end_date"`
-	MinSpend     float64   `json:"min_spend" bson:"min_spend" example:"100.0"`
-	BonusRates   float64   `json:"bonus_rates" bson:"bonus_rates" example:"8.8"`
-	AcceptedMCCs []int     `json:"accepted_mccs" bson:"accepted_mccs"`
+	UserId       string  `json:"-" bson:"user_id" example:"6400a..."`
+	CampaignId   string  `json:"campaign_id" bson:"campaign_id" example:"cmp00001"`
+	Merchant     string  `json:"merchant" bson:"merchant" example:"7-11"`
+	CardType     string  `json:"card_type" bson:"card_type" example:"super_miles_card"`
+	Description  string  `json:"description" bson:"description"`
+	StartDate    string  `json:"start_date" bson:"start_date"`
+	EndDate      string  `json:"end_date" bson:"end_date"`
+	MinSpend     float64 `json:"min_spend" bson:"min_spend" example:"100.0"`
+	BonusRates   float64 `json:"bonus_rates" bson:"bonus_rates" example:"8.8"`
+	AcceptedMCCs []int   `json:"accepted_mccs" bson:"accepted_mccs"`
 }
